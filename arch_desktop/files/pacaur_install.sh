604f6d887ac88b19779a6311135fbaf4d08b57fc
Ben Sebastian <ben@bensebastian.io>
2016-06-18 07:34:17 -0400

    updates to Arch playbook

---
+++ arch_desktop/files/pacaur_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p /tmp/pacaur_install
+cd /tmp/pacaur_install
+
+
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
+makepkg PKGBUILD --skippgpcheck
+sudo pacman -U cower*.tar.xz --noconfirm
+
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+makepkg PKGBUILD
+sudo pacman -U pacaur*.tar.xz --noconfirm
+
+cd ~
+rm -r /tmp/pacaur_install
